5985dad09f930e76bcb6117939d22f385112242a
joostine <72476229+joostine@users.noreply.github.com>
2024-05-20 15:42:39 -0700

    Importing Packaged Files

---
+++ HorrorAmusementPark/Assets/Scripts/UIScripts.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class UIScripts : MonoBehaviour
+{
+   
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+
+}
